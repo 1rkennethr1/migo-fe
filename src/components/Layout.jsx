205eaa31d3ea4e40dceb8bce149042049b411c42
Gemarlo Aquillo <ravenclaw9989@gmail.com>
2022-09-19 21:31:09 +0800

    Added Info and Layout Component
    wa ra gud

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
 	return (
-		<div className="px-[2rem] md:px-[5rem] flex flex-col justify-center min-h-screen items-center py-[10rem] 2xl:py-[12rem] ">
+		<div className="px-[2rem] md:px-[5rem] flex flex-col justify-center min-h-screen items-center pt-[10rem] 2xl:pt-[12rem] ">
 			{children}
 		</div>
 	);
