23617ec38ed735f4ace64b9ee6b46806d47165ff
Reynald Sampelo <reynalsampelo@gmail.com>
2022-09-29 12:01:12 +0800

    main routes filled

--- src/routes/main/Assess.jsx
+++ src/routes/main/Assess.jsx
@@ -2,11 +2,42 @@ import React from "react";
 import { motion } from "framer-motion";
 import Sidebar from "../../components/Sidebar";
 import MainLayout from "../../components/MainLayout";
+import { Select } from "@chakra-ui/react";
 
 const Assess = () => {
 	return (
 		<MainLayout>
-			<h1 className="text-5xl font-semibold">Assess</h1>
+			<div className="flex flex-col w-full">
+				<h1 className="text-5xl font-semibold mb-20">Assess</h1>
+				<div className="flex w-full justify-center">
+					<div className="flex flex-col w-[70%] rounded-lg shadow-lg dark:shadow-none dark:bg-[#121212] p-10 ">
+						<div className="flex items-end gap-10 mb-10">
+							<div className="flex flex-col items-start">
+								<p className="text-xs mb-2 text-gray-400">Insert data</p>
+								<Select>
+									<option selected value="">
+										Personal Data Sheet
+									</option>
+									<option value="">Exam Score</option>
+									<option value="">Personality Assessment Exam Result</option>
+									<option value="">PES Rating</option>
+									<option value="">Assigned Projects</option>
+									<option value="">Interview Results</option>
+								</Select>
+							</div>
+							<div className="flex gap-10 items-center">
+								<button className="w-[10rem] py-3 rounded-lg shadow-md">
+									Upload File
+								</button>
+								<p>No file selected</p>
+							</div>
+						</div>
+						<button className="bg-[#ec2224] text-white text-lg py-2 rounded-lg font-semibold hover:opacity-80 transition-opacity duration-300">
+							Assess
+						</button>
+					</div>
+				</div>
+			</div>
 		</MainLayout>
 	);
 };
--- src/routes/main/Dashboard.jsx
+++ src/routes/main/Dashboard.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import { motion } from "framer-motion";
 import MainLayout from "../../components/MainLayout";
-
+import image1 from "../../assets/dashboard/1.webp";
+import image2 from "../../assets/dashboard/2.webp";
+import image3 from "../../assets/dashboard/3.webp";
+import image4 from "../../assets/dashboard/4.webp";
+import image5 from "../../assets/dashboard/5.webp";
 const Dashboard = () => {
 	return (
 		<MainLayout>
-			<h1 className="text-5xl font-semibold">Alliance Inc. Dashboard</h1>
+			<div className="flex flex-col w-full">
+				<h1 className="text-5xl font-semibold mb-20">
+					Alliance Inc. Dashboard
+				</h1>
+				<div className="flex flex-col">
+					<motion.div
+						initial={{ y: 40, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						transition={{ duration: 0.5 }}
+						className="flex gap-5 justify-center mb-16"
+					>
+						<img src={image1} width={500} alt="" />
+						<img src={image2} width={500} alt="" />
+					</motion.div>
+					<motion.div
+						initial={{ y: 40, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						transition={{ duration: 0.5, delay: 0.2 }}
+						className="flex justify-center mb-10"
+					>
+						<motion.img src={image3} width={"100%"} className="" />
+					</motion.div>
+					<motion.div className="flex gap-5 justify-between mb-16">
+						<img src={image4} width={500} alt="" />
+						<img src={image5} width={500} alt="" />
+					</motion.div>
+				</div>
+			</div>
 		</MainLayout>
 	);
 };
